2c63932f54a197ee8a334a8f8f0fa1f2c21e3ed8
Santiago Saavedra <santiagosaavedra@gmail.com>
2012-01-24 12:45:42 +0100

    Added Octave and OCaml modes to emacs

--- emacs/emacs.d/site-lisp/my-autoload.el
+++ emacs/emacs.d/site-lisp/my-autoload.el
@@ -1,4 +1,5 @@
 
+
 ;; Autoloadings
 ;; Santiago Saavedra
 
@@ -11,6 +12,28 @@
 (setq common-lisp-hyperspec-root "http://localhost/clhs/HyperSpec/")
 (require 'slime)
 (slime-setup)
+
+;; OCaml-Tuareg
+(require 'tuareg)
+
+;; Octave mode on all .m files
+(autoload 'octave-mode "octave-mod" nil t)
+(setq auto-mode-alist
+      (cons '("\\.m$" . octave-mode) auto-mode-alist))
+(add-hook 'octave-mode-hook
+	  (lambda ()
+	    (abbrev-mode 1)
+	    (auto-fill-mode 1)
+	    (if (eq window-system 'x)
+		(font-lock-mode 1))))
+
+(add-hook 'inferior-octave-mode-hook
+	  (lambda ()
+	    (turn-on-font-lock)
+	    (define-key inferior-octave-mode-map [up]
+	      'comint-previous-input)
+	    (define-key inferior-octave-mode-map [down]
+	      'comint-next-input)))
 
 
 ;;; Footer.
